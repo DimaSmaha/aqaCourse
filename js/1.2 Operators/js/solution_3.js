eb9704d85caef2ad9d47a8112095d33cb8b9a1ec
DimaSmaha <dmytrosmaha@gmail.com>
2025-02-26 17:44:37 +0200

    operators task

---
+++ js/1.2 Operators/js/solution_3.js
@@ -0,0 +1,16 @@
+/** TASK 3 - Multiplication of Two Numbers
+ *
+ * 1. Declare two variables and assign them any numbers.
+ *
+ * 2. Declare another variable and assign it the result
+ * of multiplying the two previous variables.
+ *
+ * 3. Output the value of the last variable to the console.
+ */
+
+const length = 7;
+const width = 12;
+
+const area = length * width;
+
+console.log(area);
---
+++ js/1.2 Operators/js/task_3.js
@@ -0,0 +1,9 @@
+/** TASK 3 - Multiplication of Two Numbers
+ *
+ * 1. Declare two variables and assign them any numbers.
+ *
+ * 2. Declare another variable and assign it the result
+ * of multiplying the two previous variables.
+ *
+ * 3. Output the value of the last variable to the console.
+ */
