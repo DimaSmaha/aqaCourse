a33b786577f1e1fec8401177e9d66d71723cc800
DimaSmaha <dmytrosmaha@gmail.com>
2025-02-26 18:08:25 +0200

    conditions js

---
+++ js/1.3 Conditions/js/task_4.js
@@ -0,0 +1,10 @@
+/** TASK 4 - Checking Age with IF Statement
+ *
+ * 1. Declare a variable "age" and assign it any number.
+ *
+ * 2. Using an if statement, check if "age" is 18 or greater.
+ *    - If true, output "You are an adult" to the console.
+ *    - Otherwise, output "You are a minor."
+ *
+ * 3. Test the code with different values of "age."
+ */
