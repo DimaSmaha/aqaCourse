20f7bb0813ad317b744d0853d80b870a0d05cc27
DimaSmaha <dmytrosmaha@gmail.com>
2025-03-08 17:19:05 +0200

    takss pw async

---
+++ js/8. Async JS/pw/solution_13.spec.ts
@@ -0,0 +1,43 @@
+/** TASK - Using and Omitting 'await' in Playwright
+ *
+ * 1. Create a Playwright test.
+ *
+ * 2. In the 1st test create a default test for login
+ *
+ * 3. In the 2nd test create the same steps, but call the same functions without `await`.
+ *
+ * 4. Observe how the test execution differs between both cases. Why it so?
+ *
+ * 5. Use assertions to verify expected behavior.
+ *
+ * 💡 **Clue:** Playwright functions (and any functions in any frameworks) return promises, so omitting `await` may lead to unexpected results.
+ */
+
+import { test, expect } from "@playwright/test";
+
+test.describe("Suite Async Await Test", () => {
+  const usernameInput = '[data-test="username"]';
+  const passwordInput = '[data-test="password"]';
+  const loginButton = '[data-test="login-button"]';
+  const item = '[id="item_4_title_link"]';
+  const login = "standard_user";
+  const pass = "secret_sauce";
+
+  test("Async Await Test", async ({ page }) => {
+    await page.goto("/");
+    await page.locator(usernameInput).fill(login);
+    await page.locator(passwordInput).fill(pass);
+    await page.locator(passwordInput).fill(pass);
+    await page.locator(loginButton).click();
+    await expect(page.locator(item)).toBeVisible();
+  });
+
+  test("Async Await Test 2", ({ page }) => {
+    page.goto("/");
+    page.locator(usernameInput).fill(login);
+    page.locator(passwordInput).fill(pass);
+    page.locator(passwordInput).fill(pass);
+    page.locator(loginButton).click();
+    expect(page.locator(item)).toBeVisible();
+  });
+});
---
+++ js/8. Async JS/pw/task_13.spec.ts
@@ -0,0 +1,33 @@
+/** TASK - Using and Omitting 'await' in Playwright
+ *
+ * 1. Create a Playwright test.
+ *
+ * 2. In the 1st test create a default test for login
+ *
+ * 3. In the 2nd test create the same steps, but call the same functions without `await`.
+ *
+ * 4. Observe how the test execution differs between both cases. Why it so?
+ *
+ * 5. Use assertions to verify expected behavior.
+ *
+ * 💡 **Clue:** Playwright functions (and any functions in any frameworks) return promises, so omitting `await` may lead to unexpected results.
+ */
+
+import { test, expect } from "@playwright/test";
+
+test.describe("Suite Async Await Test", () => {
+  const usernameInput = '[data-test="username"]';
+  const passwordInput = '[data-test="password"]';
+  const loginButton = '[data-test="login-button"]';
+  const item = '[id="item_4_title_link"]';
+  const login = "standard_user";
+  const pass = "secret_sauce";
+
+  test("Async Await Test", async ({ page }) => {
+    //default test for login
+  });
+
+  test("Async Await Test 2", ({ page }) => {
+    // default test for login, but does not use await inside
+  });
+});
