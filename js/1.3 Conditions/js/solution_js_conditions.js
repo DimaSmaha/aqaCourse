d84737f5d844357a696535e4f89384c29ec3b1af
DimaSmaha <dmytrosmaha@gmail.com>
2025-03-09 11:51:50 +0200

    conditions update

--- js/1.3 Conditions/js/solution_js_conditions.js
+++ js/1.3 Conditions/js/solution_js_conditions.js
@@ -1,10 +1,18 @@
-/** TASK - Checking Age with IF Statement
+/** TASK - Checking Age with Conditions
  *
  * 1. Declare a variable "age" and assign it any number.
  *
- * 2. Using an if statement, check if "age" is 18 or greater.
+ * 2. Using a condtional statement, check if "age" is 18 or greater.
  *    - If true, output "You are an adult" to the console.
  *    - Otherwise, output "You are a minor."
  *
  * 3. Test the code with different values of "age."
  */
+
+const age = 20;
+
+if (age >= 18) {
+  console.log("You are an adult");
+} else {
+  console.log("You are a minor");
+}
--- js/1.3 Conditions/pw/task_pw_conditions.spec.ts
+++ js/1.3 Conditions/pw/task_pw_conditions.spec.ts
@@ -1,14 +1,16 @@
-/** TASK - Using an If Statement in a Playwright Test
+/** TASK - Using an IF Statement in a Playwright Test
  *
  * 1. Create a Playwright test.
  *
- * 2. Use an `if` statement to check a condition during the test.
+ * 2. Use an `if` statement to verify that user password is correct.
  *
  * 3. Perform different actions based on whether the condition is true or false.
  *
  * 4. Use assertions to verify the expected behavior.
  *
- * 💡 **Clue:** Create a verification for the user password, and if the password is incorrect, fail the test
+ * 💡 **Clue:** Create a verification for the user password,
+ *    - IF user password is correct, then login and verify that user is logged in
+ *    - IF the password is incorrect, fail the test by throwing error
  */
 
 import { test, expect } from "@playwright/test";
@@ -20,11 +22,20 @@ test.describe("Suite Conditions Test", () => {
   const item = '[id="item_4_title_link"]';
   const login = "standard_user";
   const pass = "secret_sauce";
+  const pass2 = "wrong_pass";
 
   test("Conditions Test", async ({ page }) => {
     await page.goto("/");
     await page.locator(usernameInput).fill(login);
     await page.locator(passwordInput).fill(pass); // verify that password Is correct
+    await page.locator(loginButton).click();
+    await expect(page.locator(item)).toBeVisible();
+  });
+
+  test("Conditions Test 2", async ({ page }) => {
+    await page.goto("/");
+    await page.locator(usernameInput).fill(login);
+    await page.locator(passwordInput).fill(pass2); // use the same if block here, and check the test behavior
     await page.locator(loginButton).click();
     await expect(page.locator(item)).toBeVisible();
   });
