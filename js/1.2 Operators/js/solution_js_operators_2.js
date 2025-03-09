e2f3a39fec70dc4b00214d0b5e06658744301eb8
DimaSmaha <dmytrosmaha@gmail.com>
2025-03-09 12:47:01 +0200

    add new operators task

---
+++ js/1.2 Operators/js/solution_js_operators_2.js
@@ -0,0 +1,27 @@
+/** TASK - Using the Increment Operator (++) in a Function
+ *
+ * 1. Declare a variable "score" and initialize it with 0.
+ *
+ * 2. Create a function "increaseScore" that:
+ *    - Increments "score" using "++".
+ *    - Logs the updated "score" value.
+ *
+ * 3. Call "increaseScore" multiple times to observe the change.
+ *
+ * 4. After the last function call, log a final score.
+ */
+
+// Your implementation goes here!
+
+let score = 0;
+
+function increaseScore() {
+  score++;
+  console.log("Current score:", score);
+}
+
+increaseScore();
+increaseScore();
+increaseScore();
+
+console.log("Final score:", score);
