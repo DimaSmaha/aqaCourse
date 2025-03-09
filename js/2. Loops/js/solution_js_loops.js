60b6f9ae3b1b618b658bd5b2493bf358839978b9
DimaSmaha <dmytrosmaha@gmail.com>
2025-03-09 11:59:18 +0200

    loops edit

--- js/2. Loops/js/solution_js_loops.js
+++ js/2. Loops/js/solution_js_loops.js
@@ -4,7 +4,7 @@
  *
  * 2. Create the variable counter and set to 0
  *
- * 3. Use a for loop to iterate from 1 to "limit":
+ * 3. Use a for loop to iterate from 0 to "limit":
  *    - In each iteration, output the current number to the console.
  *
  * 4. If counter == limit the code should finish
@@ -14,7 +14,7 @@ let counter = 0;
 const limit = 7;
 
 console.log("Counting up:");
-for (let i = 0; i <= limit; i++) {
+for (let i = 0; i < limit; i++) {
   counter++;
 
   console.log(counter);
