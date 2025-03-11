46cf7a028ed41da5b6a7beec389f4aa31c981f03
DimaSmaha <dmytrosmaha@gmail.com>
2025-03-11 21:40:35 +0200

    refactor tests

--- automation/playwright/tests/inventoryTests.spec.ts
+++ automation/playwright/tests/inventoryTests.spec.ts
@@ -1,47 +1,45 @@
-import { inventoryPage } from "./../pages/inventoryPage.page";
+import { InventoryPage } from "./../pages/inventoryPage.page";
 import { test, expect } from "@playwright/test";
-import { homePage } from "../pages/homePage.page";
+import { HomePage } from "../pages/homePage.page";
 
 test.describe("Inventory page tests", () => {
   const username = "standard_user";
   const password = "secret_sauce";
 
   test.beforeEach(async ({ page }) => {
-    let home = new homePage(page);
+    let home = new HomePage(page);
     await home.goto();
     await page.waitForLoadState();
     await home.doLogin(username, password);
   });
 
   test("Check backpack name", async ({ page }) => {
-    let inventory = new inventoryPage(page);
+    let inventory = new InventoryPage(page);
 
     await inventory.checkPageUrl();
-    expect(await inventory.backpackTitle).toBeVisible();
-    expect(await inventory.backpackTitle).toContainText("Sauce Labs Backpack");
-    expect(await inventory.backpackDescription).toContainText(
-      "carry.allTheThings()"
-    );
+    expect(inventory.backpackTitle).toBeVisible();
+    expect(inventory.backpackTitle).toContainText("Sauce Labs Backpack");
+    expect(inventory.backpackDescription).toContainText("carry.allTheThings()");
   });
 
   test("User adds backpack to the cart", async ({ page }) => {
-    let inventory = new inventoryPage(page);
+    let inventory = new InventoryPage(page);
 
     await inventory.checkPageUrl();
-    expect(await inventory.backpackTitle).toBeVisible();
+    expect(inventory.backpackTitle).toBeVisible();
     await inventory.backpackAddToCartBtn.click();
-    expect(await inventory.backpackRemoveFromCartBtn).toBeVisible();
-    expect(await inventory.cartCounter).toContainText("1");
+    expect(inventory.backpackRemoveFromCartBtn).toBeVisible();
+    expect(inventory.cartCounter).toContainText("1");
   });
 
   test("User adds  and  removes backpack to the cart", async ({ page }) => {
-    let inventory = new inventoryPage(page);
+    let inventory = new InventoryPage(page);
 
     await inventory.backpackAddToCartBtn.click();
-    expect(await inventory.backpackRemoveFromCartBtn).toBeVisible();
+    expect(inventory.backpackRemoveFromCartBtn).toBeVisible();
     await inventory.backpackRemoveFromCartBtn.click();
-    expect(await inventory.backpackRemoveFromCartBtn).not.toBeVisible();
-    expect(await inventory.backpackAddToCartBtn).toBeVisible();
-    expect(await inventory.cartCounter).not.toBeVisible();
+    expect(inventory.backpackRemoveFromCartBtn).not.toBeVisible();
+    expect(inventory.backpackAddToCartBtn).toBeVisible();
+    expect(inventory.cartCounter).not.toBeVisible();
   });
 });
