4aa3ff5cb7d34a3a826a48b8403caa3239fa6d47
DimaSmaha <dmytrosmaha@gmail.com>
2025-03-11 22:38:34 +0200

    upd ci

--- automation/playwright/smoke.config.ts
+++ automation/playwright/smoke.config.ts
@@ -20,7 +20,7 @@ const config: PlaywrightTestConfig = {
     locale: "en-GB",
     actionTimeout: 0,
     trace: "on",
-    headless: false,
+    headless: true,
     viewport: { width: 1920, height: 1080 },
     ignoreHTTPSErrors: true,
   },
